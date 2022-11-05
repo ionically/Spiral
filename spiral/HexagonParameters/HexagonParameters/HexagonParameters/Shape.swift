cf9972c5e4aa10b41087fe547d710473534401ac
ionically <chandnichaudhari.66@gmail.com>
2022-11-05 17:45:09 +0530

    First Commit

---
+++ spiral/HexagonParameters/HexagonParameters/HexagonParameters/Shape.swift
@@ -0,0 +1,27 @@
+//
+//  Shape.swift
+//  HexagonParameters
+//
+//  Created by chandni chaudhari on 05/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Shape: View {
+ 
+
+    var body: some View {
+        VStack{
+            
+            Image("shape")
+        }
+     
+    }
+}
+
+struct Shape_Previews: PreviewProvider {
+    static var previews: some View {
+        Shape()
+    }
+}
